f8df85f192004e230060d7a150bf63fa4dd0fd39
Sally Nyambura <106459262+sallynyambura@users.noreply.github.com>
2024-02-06 11:47:16 -0800

    Dropdown and display data page added

--- main.js
+++ main.js
@@ -1,67 +1,67 @@
-let teaArray = [];
 let plantArray = [];
 
-let Tea = function (tName, tType) {
-    this.name = tName;
-    this.type = tType;
-}
-
-let Plant = function (pName, pColor) {
+let PlantObject = function (pName, plightRequirement, pwaterRequirement) {
     this.name = pName;
-    this.color = pColor;
+    this.light = plightRequirement;
+    this.water =pwaterRequirement;
 }
+let waterOptions = {
+    "onceAWeek": "Once or Twice a Week",
+    "Daily": "Daily",
+    "onceAMonth": "Once or Twice a Month"
+};
+let selectedType = "";
 
+
+
+
+function addPlant() {
+    var waterRequirement = document.getElementById("select-type").value;
+    var plantName = document.getElementById("plantName").value;
+    var lightRequirement = document.getElementById("lightRequirement").value;
+
+    plantArray.push(new PlantObject(plantName, waterRequirement, lightRequirement));
+    createPlantList(); // Refresh the plant list after adding a new plant
+}
+//Runs when DOM is loaded
 document.addEventListener("DOMContentLoaded", function (event) {
-
-    createTeaList();
     createPlantList();
 
+    document.getElementById("select-type").addEventListener("change", function () {
+        updateSelectedType();
+        createPlantList();
 
-    //tea page
-    document.getElementById("addTea").addEventListener("click", function () {
-        teaArray.push ( new Tea(document.getElementById("teaName").value, 
-        document.getElementById("teaType").value ) );
-        
-        document.getElementById("teaName").value = "";
-        document.getElementById("teaType").value = "";
-
-        createTeaList();
     });
-
     //plant page
     document.getElementById("addPlant").addEventListener("click", function () {
-        plantArray.push ( new Plant(document.getElementById("plantName").value, 
-        document.getElementById("plantColor").value ) );
+        plantArray.push( new PlantObject(document.getElementById("plantName").value,
+        document.getElementById("lightRequirement").value,selectedType));
         
         document.getElementById("plantName").value = "";
-        document.getElementById("plantColor").value = "";
+        document.getElementById("lightRequirement").value = "";
 
         createPlantList();
     });
-});
 
-function createTeaList() {
-    // clear prior data
-    var teaUl = document.getElementById("teaDisplay");
-    teaUl.innerHTML = "";
-
-    teaArray.forEach(function (element,) {   // use handy array forEach method
-        var li = document.createElement('li');
-          // added data-role="listview" to the ul in the html
-        li.innerHTML = "Name: " + element.name + " Type: " + element.type;
-        teaUl.appendChild(li);
+    $(document).bind("change", "#select-type", function (event, ui) {
+        updateSelectedType();
     });
-};
+});
+function updateSelectedType() {
+    selectedType = document.getElementById("select-type").value;
+}
 
+//Function definitions
 function createPlantList() {
     // clear prior data
-    var plantUl = document.getElementById("plantDisplay");
-    plantUl.innerHTML = "";
+    var myul = document.getElementById("myul");
+    myul.innerHTML = "";
 
     plantArray.forEach(function (element,) {   // use handy array forEach method
         var li = document.createElement('li');
           // added data-role="listview" to the ul in the html
-        li.innerHTML = "Name: "+element.name + "  Color: " + element.color;
-        plantUl.appendChild(li);
+        li.innerHTML = " <strong>Plant Name:</strong>  "+element.name + "  <strong> | Light Requirement:</strong> " + element.light + " <strong>| Water Requirement:</strong> " + waterOptions[element.water];
+        myul.appendChild(li);
     });
 };
+
